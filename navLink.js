b8384682b430e074a3f10164cc2107a337b1d45a
shibichris <99465215+shibichris@users.noreply.github.com>
2023-08-10 18:51:54 +0530

    NavLink.js

---
+++ navLink.js
@@ -0,0 +1,36 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'  
+import './index.css';  
+import App from './App';  
+import About from './about'  
+import Contact from './contact'  
+  
+const routing = (  
+  <Router>  
+    <div>  
+      <h1>React Router Example</h1>  
+      <ul>  
+        <li>  
+          <NavLink to="/" exact activeStyle={  
+             {color:'red'}  
+          }>Home</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/about" exact activeStyle={  
+             {color:'green'}  
+          }>About</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/contact" exact activeStyle={  
+             {color:'magenta'}  
+          }>Contact</NavLink>  
+        </li>  
+      </ul>  
+      <Route exact path="/" component={App} />  
+      <Route path="/about" component={About} />  
+      <Route path="/contact" component={Contact} />  
+    </div>  
+  </Router>  
+)  
+ReactDOM.render(routing, document.getElementById('root')); 
